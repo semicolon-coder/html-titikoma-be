fd9fa666600e709dcf3bd46384eae3ce6dcd3096
IrvanSN <30742772+IrvanSN@users.noreply.github.com>
2022-03-06 11:08:01 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+      colors: {
+        'main': '#498CFF',
+        'second': '#2D3C8F',
+        'white': '#FFFFFF',
+        'black': '#272838'
+      },
+    },
+  },
+  plugins: [],
+}
